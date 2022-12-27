44f02ccfd03830adbfe5b0d7970f4aa76f4188db
votanloccntt <votanloccntt@gmail.com>
2022-12-27 16:20:31 +0700

    create header

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Tan Loc Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header></Header>
+      {/* Banner */}
+    </div>
+  );
+};
+
+export default Home;
